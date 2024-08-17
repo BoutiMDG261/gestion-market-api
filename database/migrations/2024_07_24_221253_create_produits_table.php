9746039041028af6fd433fc5ce72296cd4015a7b
BoutiMDG261 <ramiandra.marco@outlook.com>
2024-08-17 13:50:01 +0300

    A-005: refoctorisation auth

--- database/migrations/2024_07_24_221253_create_produits_table.php
+++ database/migrations/2024_07_24_221253_create_produits_table.php
@@ -18,7 +18,14 @@ class CreateProduitsTable extends Migration
             $table->string('name')->unique();
             $table->text('description')->nullable();
             $table->decimal('price', 8, 2);
+            $table->unsignedBigInteger('created_by')->nullable(); // Ajout du champ created_by
+            $table->unsignedBigInteger('updated_by')->nullable(); // Ajout du champ updated_by
             $table->timestamps();
+            $table->softDeletes();
+
+            // Clés étrangères
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
         });
     }
 
